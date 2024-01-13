ad920c3cd59d80c67eb0f8c425734ec968e24892
taimatu7 <taimatu7@outlook.jp>
2024-01-13 20:16:59 +0900

    update: 店舗登録画面のレイアウトを追加する

---
+++ lib/presentation/constants/constants.dart
@@ -0,0 +1,11 @@
+// 共通
+import 'package:flutter/material.dart';
+
+const screenPaddingSize = 15.0;
+const titleColor = Colors.white;
+const appBarColor = Colors.blue;
+const bottonTextColor = Colors.white;
+
+// 店舗登録画面
+const inputLabelFontSizeStore = 20.0;
+const buttonFontSizeStore = 20.0;
--- lib/presentation/registration/store/view/store_screen.dart
+++ lib/presentation/registration/store/view/store_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:free_market_pos_flutter/presentation/constants/constants.dart';
 import 'package:free_market_pos_flutter/presentation/sale/sale/view/sale_screen.dart';
 
 class StoreScreen extends StatefulWidget {
@@ -10,20 +11,54 @@ class StoreScreen extends StatefulWidget {
 class _StoreScreenState extends State<StoreScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          children: [
-            const Text("StoreScreen"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const SaleScreen();
-                  }));
-                },
-                child: const Text("次の画面"))
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: const Text("店舗登録", style: TextStyle(color: titleColor)),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(screenPaddingSize),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("店舗名を入力してください。",
+                  style: TextStyle(fontSize: inputLabelFontSizeStore)),
+              const SizedBox(
+                height: 20,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '店舗名',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const SaleScreen();
+                    }));
+                  },
+                  child: const Text("登録",
+                      style: TextStyle(
+                          color: bottonTextColor,
+                          fontSize: buttonFontSizeStore)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
