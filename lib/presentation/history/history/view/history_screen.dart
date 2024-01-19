a5f3af95415d7256c14cdd191d7f7ca6df0f5247
taimatu7 <taimatu7@outlook.jp>
2024-01-20 06:42:21 +0900

    update: 履歴画面のレイアウト修正

--- lib/presentation/history/history/view/history_screen.dart
+++ lib/presentation/history/history/view/history_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:free_market_pos_flutter/presentation/components/side_drawer/side_drawer.dart';
+import 'package:free_market_pos_flutter/presentation/constants/constants.dart';
+import 'package:free_market_pos_flutter/presentation/history/history/view/widgets/history_detail_dialog.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -8,16 +10,61 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
+  final List<
+      ({
+        int id,
+        String datetime,
+        List<String> itemList,
+        String quantity,
+        String totalPrice
+      })> list = [
+    (
+      id: 1,
+      datetime: "2024/01/20 9:32",
+      itemList: ["革ジャン", "コート"],
+      quantity: "3",
+      totalPrice: "¥3,0000"
+    ),
+    (
+      id: 2,
+      datetime: "2024/01/20 10:30",
+      itemList: ["スニーカー", "ワンピース", "ハイヒール"],
+      quantity: "3",
+      totalPrice: "¥2,0000"
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('販売履歴'),
-      ),
-      drawer: const SideDrawer(),
-      body: const Center(
-        child: Text("HistoryScreen"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("会計", style: TextStyle(color: titleColor)),
+          backgroundColor: appBarColor,
+          centerTitle: true,
+        ),
+        drawer: const SideDrawer(),
+        body: ListView.separated(
+            itemBuilder: (_, int index) {
+              return ListTile(
+                  leading: Text(list[index].id.toString()),
+                  title: Text(list[index].itemList.join(","),
+                      overflow: TextOverflow.ellipsis),
+                  subtitle: Row(
+                    children: [
+                      Text(list[index].datetime),
+                      const SizedBox(width: 10),
+                      Text("個数:${list[index].quantity}"),
+                    ],
+                  ),
+                  trailing: Text(list[index].totalPrice),
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return const HistoryDetalDialog();
+                        });
+                  });
+            },
+            separatorBuilder: (_, int index) => const Divider(height: 0),
+            itemCount: list.length));
   }
 }
