80942ba7c80cabe609bcbadaddd0bc27ab7af0e8
taimatu7 <taimatu7@outlook.jp>
2024-01-20 06:44:12 +0900

    update: 設定画面のレイアウト変更

--- lib/presentation/settings/settings/view/settings_screen.dart
+++ lib/presentation/settings/settings/view/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:free_market_pos_flutter/presentation/components/side_drawer/side_drawer.dart';
+import 'package:free_market_pos_flutter/presentation/constants/constants.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -11,13 +12,25 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('設定'),
-      ),
-      drawer: const SideDrawer(),
-      body: const Center(
-        child: Text("SettingsScreen"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("設定", style: TextStyle(color: titleColor)),
+          backgroundColor: appBarColor,
+          centerTitle: true,
+        ),
+        drawer: const SideDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListView(
+            children: const [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Version"),
+                  Text("1.0.0"),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
