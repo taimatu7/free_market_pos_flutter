99822ad31dbb994e5a27996426053f8436c1df3e
taimatu7 <taimatu7@outlook.jp>
2024-01-21 06:29:03 +0900

    update: 設定画面のレイアウトを変更

--- lib/presentation/settings/settings/view/settings_screen.dart
+++ lib/presentation/settings/settings/view/settings_screen.dart
@@ -9,6 +9,14 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final _storeNameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _storeNameController.text = "test店";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +26,66 @@ class _SettingsScreenState extends State<SettingsScreen> {
           centerTitle: true,
         ),
         drawer: const SideDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ListView(
-            children: const [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Version"),
-                  Text("1.0.0"),
-                ],
-              )
-            ],
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ListView(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("店舗名"), Text("test店")],
+                ),
+                const Divider(),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Version"),
+                    Text("1.0.0"),
+                  ],
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.blue),
+                        ),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (_) {
+                                return AlertDialog(
+                                  title: const Text("店舗名を変更"),
+                                  content: TextField(
+                                    controller: _storeNameController,
+                                    autofocus: true,
+                                    decoration: const InputDecoration(
+                                      labelText: '店舗名',
+                                      hintText: '店舗名を入力してください',
+                                    ),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('Cancel'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: const Text('Save'),
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                        child: const Text("店舗名変更",
+                            style: TextStyle(color: bottonTextColor))))
+              ],
+            ),
           ),
         ));
   }
